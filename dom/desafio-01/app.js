ce797267d0c7dc76385c5f4c76e83a1e966fe13b
Daniel Alexandre Cordeiro <daniel@sim2701.simeparnet>
2020-09-03 23:17:07 -0300

    Updated
    Signed-off-by: Daniel Alexandre Cordeiro <daniel@sim2701.simeparnet>

--- dom/desafio-01/app.js
+++ dom/desafio-01/app.js
@@ -1,11 +1,14 @@
 new Vue({
     el: '#desafio',
     data: {
-        nome: 'Daniel',
-        idade: 42,
+        nome: 'Maria',
+        idade: 29,
         url_image: 'https://i.pinimg.com/280x280_RS/35/c3/99/35c39967709bf375b52eba98f8d9f035.jpg'
     },
     methods: {
+        idadeVezes(x) {
+            return this.idade*x
+        },
         random() {
             return Math.random()
         }
