744b4a6df0fedf82a9decca50eb14aa66790b506
Daniel Alexandre Cordeiro <daniel@sim2701.simeparnet>
2020-09-14 02:04:22 -0300

    Implementado Atacar / Resultado
    Signed-off-by: Daniel Alexandre Cordeiro <daniel@sim2701.simeparnet>

--- matador-mostro/app.js
+++ matador-mostro/app.js
@@ -2,26 +2,76 @@ new Vue({
 	el: '#main',
 	data: {
 		iniciado: false,
-		load_jogador: 100,
-		load_mostro: 100
+		load_jogador: 100,		
+		load_mostro: 100,
+		width_jogador: '100%',
+		width_mostro: '100%',
+		mostrar_mensagem: false,
+		resultado: ''
 	},
 	methods: {
+		iniciar() {
+			this.iniciado = true,
+			this.load_jogador = 100,
+			this.load_mostro = 100,
+			this.width_jogador = '100%',
+			this.width_mostro = '100%',
+			this.resultado = '',
+			this.mostrar_mensagem = false
+		},
 		desistir() {
 			this.iniciado = false,
 			this.load_jogador = 100,
-			this.load_mostro = 100
+			this.load_mostro = 100,
+			this.width_jogador = '100%',
+			this.width_mostro = '100%',
+			this.resultado = '',
+			this.mostrar_mensagem = false
+		},
+		atacar() {
+			this.load_jogador -= randomIntFromInterval(9,15)			
+			this.load_mostro -= randomIntFromInterval(5,9)
+
+			if (this.load_jogador < 0) this.load_jogador = 0
+			if (this.load_mostro < 0) this.load_mostro = 0
+
+			this.width_jogador = `${this.load_jogador}%`
+			this.width_mostro = `${this.load_mostro}%`
+
+			this.verificar_resultado()
+		},
+		verificar_resultado() {			
+			if (this.load_jogador == 0 && this.load_mostro > 0) {
+				this.iniciado = false
+				this.mostrar_mensagem = true,
+				this.resultado = 'Monstro ganhou!'
+			}
+			else if (this.load_jogador > 0 && this.load_mostro == 0) {
+				this.iniciado = false
+				this.mostrar_mensagem = true,
+				this.resultado = 'Você ganhou!'
+			}
+			else if (this.load_jogador > 0 && this.load_mostro == 0) {
+				this.iniciado = false
+				this.mostrar_mensagem = true,
+				this.resultado = 'Empataram!'
+			}
 		}
 	},
     computed: {
         progress_jogador() {
-			return {
-				progressFull: (this.load_jogador == 100 ? true : false)
+			return {			
+				progressOk: (this.load_jogador > 20 ? true : false)
 			}
 		},
 		progress_mostro() {
 			return {
-				progressFull: (this.load_mostro == 100 ? true : false)
+				progressOk: (this.load_mostro > 20 ? true : false)
 			}
-		}
+		}		
     }
 });
+
+function randomIntFromInterval(min, max) { // min and max included 
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
